382b29cbb10dd6136fd19a866d6b7bd5d394c0f9
david30-maker <davidokpah396@gmail.com>
2024-09-11 10:05:23 +0100

    add controller methods and routes

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Put, Post } from '@nestjs/common';
 
 @Controller('songs')
 export class SongsController {
+  @Post()
+  create() {
+    return 'Create a new song endpoint';
+  }
+
   @Get()
   findAll() {
-    return 'Find all songs';
+    return 'Find all songs endpoint';
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'Fetch one song on the basis of id';
+  }
+
+  @Put(':id')
+  update() {
+    return 'Update song on the basis of id';
+  }
+
+  @Delete(':id')
+  remove() {
+    return 'Delete song on the basis of id';
   }
 }

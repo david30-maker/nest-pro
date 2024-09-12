8c634b9cc7da3e5b7e11ef3b46f9fdd2823d416e
david30-maker <davidokpah396@gmail.com>
2024-09-12 21:53:41 +0100

    create different kind of providers

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
-
 @Module({
   controllers: [SongsController],
-  providers: [SongsService],
+  providers: [
+    SongsService,
+    // {
+    //   provide: SongsService,
+    //   useClass: SongsService,
+    // },
+    // {
+    //   provide: 'SongsService',
+    //   useValue: mockSongsService,
+    // },
+  ],
 })
 export class SongsModule {}
